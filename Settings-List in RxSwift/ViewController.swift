4275801f9918db781ae497aaa54da6794d903d79
sageibra <akaev_ibragim@mail.ru>
2021-03-16 00:40:31 +0300

    Setup TableView using RxSwift

--- Settings-List in RxSwift/ViewController.swift
+++ Settings-List in RxSwift/ViewController.swift
@@ -6,29 +6,36 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIScrollViewDelegate {
+    
     @IBOutlet var tableView: UITableView!
-    let settings = [
-        Settings(name: "General", icon: UIImage(systemName: "gear")!),
-        Settings(name: "Control Center", icon: UIImage(systemName: "switch.2")!),
-        Settings(name: "Display & Brightness", icon: UIImage(systemName: "textformat.size")!),
-        Settings(name: "Home Screen", icon: UIImage(systemName: "gear")!),
-        Settings(name: "Accessibility", icon: UIImage(systemName: "gear")!),
-        Settings(name: "Wallpaper", icon: UIImage(systemName: "gear")!),
-        Settings(name: "Siri & Search", icon: UIImage(systemName: "gear")!),
-        Settings(name: "Touch ID & Passcode", icon: UIImage(systemName: "gear")!),
-        Settings(name: "Emergency SOS", icon: UIImage(systemName: "gear")!),
-        Settings(name: "Exposure Notifications", icon: UIImage(systemName: "gear")!),
-        Settings(name: "Battery", icon: UIImage(systemName: "gear")!),
-        Settings(name: "Privacy", icon: UIImage(systemName: "gear")!)
-    ]
+    private let bag = DisposeBag()
+    private let settings = Observable.just([
+        Settings(name: "General", icon: Images.general),
+        Settings(name: "Control Center", icon: Images.controlCenter),
+        Settings(name: "Display & Brightness", icon: Images.displayBrightness),
+        Settings(name: "Accessibility", icon: Images.accessibility),
+        Settings(name: "Wallpaper", icon: Images.wallpaper),
+        Settings(name: "Touch ID & Passcode", icon: Images.touchID),
+        Settings(name: "Battery", icon: Images.battery),
+        Settings(name: "Privacy", icon: Images.privacy)
+    ])
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        navigationController?.navigationBar.prefersLargeTitles = true
+        configureTableView()
     }
-
-
+    
+    func configureTableView() {
+        tableView.tableFooterView = UIView(frame: .zero)
+        settings.bind(to: tableView.rx.items(cellIdentifier: "cell")) { index, settings, cell in
+            cell.textLabel?.text = settings.name
+            cell.imageView?.image = settings.icon
+        }.disposed(by: bag)
+    }
 }
 
