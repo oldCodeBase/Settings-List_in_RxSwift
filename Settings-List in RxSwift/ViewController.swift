8db94d5b2749edb9e4bfd40448f7ea549594fddf
sageibra <akaev_ibragim@mail.ru>
2021-03-15 23:39:05 +0300

    Initial Commit

---
+++ Settings-List in RxSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Settings-List in RxSwift
+//
+//  Created by Ibragim Akaev on 15/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
