53adef89c66eef2ebc263bedd913389c82d692f6
sageibra <akaev_ibragim@mail.ru>
2021-03-15 23:59:41 +0300

    Add array of settings

--- Settings-List in RxSwift/ViewController.swift
+++ Settings-List in RxSwift/ViewController.swift
@@ -9,9 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var tableView: UITableView!
+    let settings = [
+        Settings(name: "General", icon: UIImage(systemName: "gear")!),
+        Settings(name: "Control Center", icon: UIImage(systemName: "switch.2")!),
+        Settings(name: "Display & Brightness", icon: UIImage(systemName: "textformat.size")!),
+        Settings(name: "Home Screen", icon: UIImage(systemName: "gear")!),
+        Settings(name: "Accessibility", icon: UIImage(systemName: "gear")!),
+        Settings(name: "Wallpaper", icon: UIImage(systemName: "gear")!),
+        Settings(name: "Siri & Search", icon: UIImage(systemName: "gear")!),
+        Settings(name: "Touch ID & Passcode", icon: UIImage(systemName: "gear")!),
+        Settings(name: "Emergency SOS", icon: UIImage(systemName: "gear")!),
+        Settings(name: "Exposure Notifications", icon: UIImage(systemName: "gear")!),
+        Settings(name: "Battery", icon: UIImage(systemName: "gear")!),
+        Settings(name: "Privacy", icon: UIImage(systemName: "gear")!)
+    ]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
