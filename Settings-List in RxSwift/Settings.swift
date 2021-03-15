5301af172d34be16c87b60cc0d01388f88c102bc
sageibra <akaev_ibragim@mail.ru>
2021-03-15 23:59:15 +0300

    Create settings model

---
+++ Settings-List in RxSwift/Settings.swift
@@ -0,0 +1,13 @@
+//
+//  Settings.swift
+//  Settings-List in RxSwift
+//
+//  Created by Ibragim Akaev on 15/03/2021.
+//
+
+import UIKit
+
+struct Settings {
+    let name: String
+    let icon: UIImage
+}
